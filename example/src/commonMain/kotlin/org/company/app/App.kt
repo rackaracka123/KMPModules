6a6bef2640fdf935ad3b72472c8543433e13b816
Mattster <mats-hjalmar.murman@alster.se>
2024-02-01 12:46:31 +0100

    Hello World example of player

--- example/src/commonMain/kotlin/org/company/app/App.kt
+++ example/src/commonMain/kotlin/org/company/app/App.kt
@@ -2,10 +2,29 @@ package org.company.app
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import se.alster.kmp.media.camera.CameraView
+import se.alster.kmp.media.player.AspectRatio
+import se.alster.kmp.media.player.PlayerView
+import se.alster.kmp.media.player.Track
+import se.alster.kmp.media.player.rememberPlayer
 
 @Composable
 internal fun App() {
-    CameraView(modifier = Modifier.fillMaxSize(), {}, {})
+    val player = rememberPlayer()
+
+    PlayerView(
+        modifier = Modifier.fillMaxSize(),
+        player = player,
+        aspectRatio = AspectRatio.FitWithAspectRatio,
+    )
+    DisposableEffect(Unit) {
+        player.prepareTrackForPlayback(
+            Track.Mp4Vod("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4")
+        )
+        onDispose {
+            player.release()
+        }
+    }
 }
